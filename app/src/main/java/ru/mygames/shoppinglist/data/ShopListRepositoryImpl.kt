0e9cc338b04bcda4996a8aca81f51542f48430e8
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-03-09 10:06:45 +0300

    пока добавил init для проверки работы программы

--- app/src/main/java/ru/mygames/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/ru/mygames/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -4,13 +4,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import ru.mygames.shoppinglist.domain.ShopItem
 import ru.mygames.shoppinglist.domain.ShopListRepository
+import kotlin.random.Random
 
 object ShopListRepositoryImpl: ShopListRepository {
-    private var shopList = mutableListOf<ShopItem>()
+    private var shopList = sortedSetOf<ShopItem>({o1, o2 -> o1.id.compareTo(o2.id)})
 
     private val shopListLiveData = MutableLiveData<List<ShopItem>>()
 
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 1000){
+            val shopItem = ShopItem( "Name $i", i,Random.nextBoolean())
+            addShopItem(shopItem)
+        }
+    }
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFINED_ID)
             shopItem.id = autoIncrementId++
