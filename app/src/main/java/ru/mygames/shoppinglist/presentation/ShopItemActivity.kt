5bc364e744138bd863c0f512fc79ee4e54d38b34
jesterking <kotelnikov.dmitrrii20@yandex.ru>
2025-03-14 10:28:30 +0300

    баг пофикшен

--- app/src/main/java/ru/mygames/shoppinglist/presentation/ShopItemActivity.kt
+++ app/src/main/java/ru/mygames/shoppinglist/presentation/ShopItemActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.textfield.TextInputLayout
@@ -14,38 +15,38 @@ class ShopItemActivity:AppCompatActivity() {
     private lateinit var viewModel: ShopItemViewModel
     private lateinit var tilName: TextInputLayout
     private lateinit var tilCount: TextInputLayout
-    private lateinit var etName: TextInputLayout
-    private lateinit var etCount: TextInputLayout
+    private lateinit var etName: EditText
+    private lateinit var etCount: EditText
     private lateinit var buttonSave: Button
     private var screenMode = ""
     private var shopItemId = ShopItem.UNDEFINED_ID
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shop_item)
-        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-//        parseIntent()
+
+        parseIntent()
         viewModel = ViewModelProvider(this)[ShopItemViewModel::class.java]
-//        initValues()
+        initValues()
     }
-//    private fun parseIntent(){
-//        if (!intent.hasExtra(EXTRA_SCREEN_MODE))
-//            throw RuntimeException("Param screen mode is absent")
-//        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
-//        if (mode != MODE_EDIT && mode != MODE_ADD)
-//            throw RuntimeException("Invalid screen mode: $mode")
-//        if (mode == MODE_EDIT){
-//            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID))
-//                throw RuntimeException("Param shop item id is absent")
-//            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
-//        }
-//    }
-//    private fun initValues(){
-//         tilName = findViewById(R.id.til_name)
-//         tilCount = findViewById(R.id.til_count)
-//         etName = findViewById(R.id.et_name)
-//         etCount = findViewById(R.id.et_count)
-//         buttonSave = findViewById(R.id.save_button)
-//    }
+    private fun parseIntent(){
+        if (!intent.hasExtra(EXTRA_SCREEN_MODE))
+            throw RuntimeException("Param screen mode is absent")
+        val mode = intent.getStringExtra(EXTRA_SCREEN_MODE)
+        if (mode != MODE_EDIT && mode != MODE_ADD)
+            throw RuntimeException("Invalid screen mode: $mode")
+        if (mode == MODE_EDIT){
+            if (!intent.hasExtra(EXTRA_SHOP_ITEM_ID))
+                throw RuntimeException("Param shop item id is absent")
+            shopItemId = intent.getIntExtra(EXTRA_SHOP_ITEM_ID, ShopItem.UNDEFINED_ID)
+        }
+    }
+    private fun initValues(){
+         tilName = findViewById(R.id.til_name)
+         tilCount = findViewById(R.id.til_count)
+         etName = findViewById(R.id.et_name)
+         etCount = findViewById(R.id.et_count)
+         buttonSave = findViewById(R.id.save_button)
+    }
 
 
 
